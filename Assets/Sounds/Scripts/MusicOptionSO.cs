0f7953faaa2d8e0290b2e3b3f8750310dcf95aac
Andrey <andrey-199213@mail.ru>
2021-04-25 20:53:14 +0300

    del Music

--- Assets/Sounds/Scripts/MusicOptionSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "MusicOption")]
-public class MusicOptionSO : ScriptableObject
-{
-    public MusicType type;
-    public bool play;
-    [Range(0, 1)] public float volume;
-}
